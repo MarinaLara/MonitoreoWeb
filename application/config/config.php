4fb98c23e56dceb8fa7db3b5c10c35c084220bef
MarinaLara <labrujadelcuentoxd@gmail.com>
2022-12-07 14:44:42 -0700

    Revert "Update config.php"
    This reverts commit f769a84bb60f77334048dcb635d8c172f69bf929.

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'https://localhost/MonitoreoWeb';
-$config['base_url'] = 'https://pruebahostmonitor.web.app/';
+$config['base_url'] = 'https://localhost/MonitoreoWeb';
 
 /*
 |--------------------------------------------------------------------------
