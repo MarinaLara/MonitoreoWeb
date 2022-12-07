c3033c9e603949dc758fe6762892b3aa2e2e93de
MarinaLara <labrujadelcuentoxd@gmail.com>
2022-12-07 14:45:11 -0700

    Revert "firebase y github"
    This reverts commit db4967006bdca26f3733c31bdf5e153bc62d1d2b.

--- application/views/footers/footer.php
+++ application/views/footers/footer.php
@@ -1,79 +1,59 @@
-</div>
-<!-- jQuery -->
-<script src="<?= base_url() ?>template/admin-lte/plugins/jquery/jquery.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="<?= base_url() ?>template/admin-lte/plugins/jquery-ui/jquery-ui.min.js"></script>
-
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-	$.widget.bridge('uibutton', $.ui.button)
-</script>
-<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.js"></script>
-<!-- boostrap -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-	integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-	crossorigin="anonymous"></script>
-<!-- AdminLTE -->
-<script src="<?= base_url(); ?>template/admin-lte/dist/js/adminlte.js"></script>
-<!-- sweetalert2 LTE -->
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<!-- <script defer src="<?= base_url(); ?>template/admin-lte/plugins/sweetalert2/sweetalert2.all.min.js"></script>
+        </div>
+        <!-- jQuery -->
+        <script src="<?= base_url() ?>template/admin-lte/plugins/jquery/jquery.min.js"></script>
+        <!-- jQuery UI 1.11.4 -->
+        <script src="<?= base_url() ?>template/admin-lte/plugins/jquery-ui/jquery-ui.min.js"></script>
+        
+        <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+        <script>
+            $.widget.bridge('uibutton', $.ui.button)
+        </script>
+        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.js"></script>
+        <!-- boostrap -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+        crossorigin="anonymous"></script>
+        <!-- AdminLTE -->
+		<script  src="<?= base_url(); ?>template/admin-lte/dist/js/adminlte.js"></script>
+		<!-- sweetalert2 LTE -->
+		<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+		<!-- <script defer src="<?= base_url(); ?>template/admin-lte/plugins/sweetalert2/sweetalert2.all.min.js"></script>
 		<script defer src="<?= base_url(); ?>template/admin-lte/plugins/sweetalert2/sweetalert2.min.js"></script> -->
-<script type="module">
-	// Import the functions you need from the SDKs you need
-	import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-	// TODO: Add SDKs for Firebase products that you want to use
-	// https://firebase.google.com/docs/web/setup#available-libraries
-
-	// Your web app's Firebase configuration
-	const firebaseConfig = {
-		apiKey: "AIzaSyAlhmNkfGJm1V3T1uQFOpz3529h4e-3wLE",
-		authDomain: "monitoreoabejas.firebaseapp.com",
-		projectId: "monitoreoabejas",
-		storageBucket: "monitoreoabejas.appspot.com",
-		messagingSenderId: "154807912320",
-		appId: "1:154807912320:web:0303656c8c57d6fe966365"
-	};
-
-	// Initialize Firebase
-	const app = initializeApp(firebaseConfig);
-</script>
-<script>
-	// FUNCION PARA CARGAR AJAX DESDE CUALQUIER ARCHIVO JS o <script defer> DEL SISTEMA
-	var cargar_ajax = {
-		run_server_ajax: function (_url, _data = null) {
-			var json_result = $.ajax({
-				url: '<?= base_url(); ?>' + _url,
-				dataType: "json",
-				method: "post",
-				async: false,
-				type: 'post',
-				data: _data,
-				done: function (response) {
-					return response;
+        <script>
+            // FUNCION PARA CARGAR AJAX DESDE CUALQUIER ARCHIVO JS o <script defer> DEL SISTEMA
+			var cargar_ajax = {
+				run_server_ajax: function(_url, _data = null) {
+					var json_result = $.ajax({
+						url: '<?= base_url(); ?>' + _url,
+						dataType: "json",
+						method: "post",
+						async: false,
+						type: 'post',
+						data: _data,
+						done: function(response) {
+							return response;
+						}
+					}).responseJSON;
+					return json_result;
 				}
-			}).responseJSON;
-			return json_result;
-		}
-	}
-	$(document).ready(function () {
-		var winWidth = $(window).width();
-		$(window).on('resize', function () {
-			var winWidth = $(window).width();
-			if (winWidth < 576) {
-				console.log('Window Width: ' + winWidth + 'class used: col');
-			} else if (winWidth < 768) {
-				console.log('Window Width: ' + winWidth + 'class used: col-sm');
-			} else if (winWidth < 992) {
-				console.log('Window Width: ' + winWidth + 'class used: col-md');
-			} else if (winWidth < 1200) {
-				console.log('Window Width: ' + winWidth + 'class used: col-lg');
-			} else {
-				console.log('Window Width: ' + winWidth + 'class used: col-xl');
 			}
-		});
-	});
-</script>
-</body>
-
+            $(document).ready(function() {
+				var winWidth = $(window).width();
+				$(window).on('resize', function() {
+					var winWidth = $(window).width();
+					if (winWidth < 576) {
+						console.log('Window Width: ' + winWidth + 'class used: col');
+					} else if (winWidth < 768) {
+						console.log('Window Width: ' + winWidth + 'class used: col-sm');
+					} else if (winWidth < 992) {
+						console.log('Window Width: ' + winWidth + 'class used: col-md');
+					} else if (winWidth < 1200) {
+						console.log('Window Width: ' + winWidth + 'class used: col-lg');
+					} else {
+						console.log('Window Width: ' + winWidth + 'class used: col-xl');
+					}
+				});
+			});
+        </script>
+    </body>
 </html>
